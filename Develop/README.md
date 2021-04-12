7b6eed7580ddd56998e501c040f521e11b911ab6
Jovan Nixon <jmnfire@gmail.com>
2021-04-11 23:03:00 -0600

    Update 1

---
+++ Develop/README.md
@@ -0,0 +1,30 @@
+
+## GitHub User name 
+jmnfier
+
+## Email address 
+jadljfal
+
+# Project Title 
+jfadlja
+
+## Description 
+jdlfjad
+
+## License
+![badge](https://img.shields.io/badge/license-GPL-3.0-brightgreen)
+
+This application is covered by the GPL 3.0 license. 
+
+## Dependencies
+jfhalsf
+
+## Tests
+ljadfjhadf
+
+## Usage
+jfladjf
+
+## Code
+![Changed HTML](./assets/images/code1.png)
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,106 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+    {
+        type: 'input',
+        message: 'GitHub username?',
+        name: 'userName',
+    },
+    {
+        type: 'input',
+        message: 'What is you email address?',
+        name: 'address',
+    }, {
+        type: 'input',
+        message: 'What is your project tile?',
+        name: 'project',
+    }, {
+        type: 'input',
+        message: 'What is a brief description of your project?',
+        name: 'briefProject'
+    }, {
+        type: 'list',
+        name: 'license',
+        message: 'What license should your project have?',
+        choices: [
+                'MIT',
+                'Apache 2.0',
+                'GPL 3.0',
+                'BSD 3',
+                'none',
+        ]              
+    }, {
+        type: 'input',
+        message: 'What should be run to install dependencies?',
+        name: 'dependencies'
+    }, {
+        type: 'input',
+        message: 'What should be run to run tests?',
+        name: 'tests'
+    }, {
+        type: 'input',
+        message: 'What does your user need to know about using this repo?',
+        name: 'repo'
+    }, {
+        type: 'input',
+        message: 'What does user need to know about contributing to repo',
+        name: 'contributing'
+    },
+])
+
+.then((questions) => 
+
+    fs.writeFile('README.md',
+`
+## GitHub User name 
+${questions.userName}
+
+## Email address 
+${questions.address}
+
+# Project Title 
+${questions.project}
+
+## Description 
+${questions.briefProject}
+
+## License
+![badge](https://img.shields.io/badge/license-${questions.license}-brightgreen)
+
+This application is covered by the ${questions.license} license. 
+
+## Dependencies
+${questions.dependencies}
+
+## Tests
+${questions.tests}
+
+## Usage
+${questions.repo}
+
+## Code
+![Changed HTML](./assets/images/code1.png)
+
+`, (err) =>
+    err ? console.log(err) : console.log('README Created!'))
+);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// const init = () => {
+//     inquirer.prompt(questions).then((response) => {
+//         var generateReadme = readme(response)
+//         fs.writeFile('readme.md', generateReadme, (err) =>
+//             err ? console.error(err) : console.log('README Created'))
+//     }) .catch(err => console.log(err))    
+// }
 
 // Function call to initialize app
-init();
+// init();
